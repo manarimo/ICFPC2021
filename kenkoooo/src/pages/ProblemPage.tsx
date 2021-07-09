6293b3b1582eb2e5583a60b09c4b7ec393eb86e9
kenkoooo <kenkou.n@gmail.com>
2021-07-10 01:41:17 +0900

    ウェブアプリを移した

---
+++ kenkoooo/src/pages/ProblemPage.tsx
@@ -0,0 +1,67 @@
+import React, { useState } from "react";
+import { useParams } from "react-router-dom";
+import { Problem, useProblemData } from "../utils";
+import { Alert, Container, Spinner } from "react-bootstrap";
+import { SvgViewer } from "./SvgViewer";
+import { EditorState } from "./EditorState";
+
+interface SvgEditorProps {
+  problem: Problem;
+}
+const SvgEditor = (props: SvgEditorProps) => {
+  const { problem } = props;
+  const [editorState, setEditState] = useState<EditorState | null>(null);
+  const [userFigure, setUserFigure] = useState({ ...problem.figure });
+  return (
+    <SvgViewer
+      userFigure={userFigure}
+      problem={problem}
+      onEdit={(pointId) => {
+        if (!editorState) {
+          setEditState({ pointId });
+        }
+      }}
+      onLatticeTouch={([x, y]) => {
+        if (editorState) {
+          const pointId = editorState.pointId;
+          const [curX, curY] = userFigure.vertices[pointId];
+          if (curX !== x || curY !== y) {
+            const newFigure = { ...userFigure };
+            newFigure.vertices[pointId] = [x, y];
+            setUserFigure(newFigure);
+          }
+        }
+      }}
+      onMouseUp={() => {
+        if (editorState) {
+          setEditState(null);
+        }
+      }}
+      editorState={editorState}
+    />
+  );
+};
+
+export const ProblemPage = () => {
+  const { problemId } = useParams<{ problemId: string }>();
+  const problem = useProblemData(problemId);
+
+  if (problem.error) {
+    console.error(problem.error);
+    return <Alert variant="danger">{problem.error}</Alert>;
+  }
+
+  if (!problem.data) {
+    return (
+      <Spinner animation="border" role="status">
+        <span className="sr-only">Loading...</span>
+      </Spinner>
+    );
+  }
+
+  return (
+    <Container>
+      <SvgEditor problem={problem.data} />
+    </Container>
+  );
+};
---
+++ kenkoooo/src/utils.ts
@@ -0,0 +1,36 @@
+import useSWR, { SWRConfiguration } from "swr";
+
+const API_SERVER = process.env.REACT_APP_API_SERVER;
+
+export const useSWRData = <T>(
+  url: string,
+  fetcher: (url: string) => Promise<T>,
+  config: SWRConfiguration<T> = {}
+) => {
+  return useSWR(url, fetcher, {
+    revalidateOnFocus: false,
+    revalidateOnReconnect: false,
+    refreshWhenHidden: true,
+    ...config,
+  });
+};
+
+export const useProblemData = (problemId: string) => {
+  const url = `${API_SERVER}/${problemId}.json`;
+  const fetcher = (url: string) =>
+    fetch(url)
+      .then((response) => response.json())
+      .then((problem) => problem as Problem);
+  return useSWRData(url, fetcher);
+};
+
+export interface Problem {
+  hole: [number, number][];
+  epsilon: number;
+  figure: Figure;
+}
+
+export interface Figure {
+  edges: [number, number][];
+  vertices: [number, number][];
+}
