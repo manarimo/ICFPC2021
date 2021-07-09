caca6ab46db2d3e72081243633a88a872ca28261
kenkoooo <kenkou.n@gmail.com>
2021-07-10 04:47:42 +0900

    visualizer: 関数を移動

--- kenkoooo/src/pages/ProblemPage.tsx
+++ kenkoooo/src/pages/ProblemPage.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useParams } from "react-router-dom";
-import { hasOwnProperty, Problem, useProblemData } from "../utils";
+import { parseUserInput, Problem, useProblemData } from "../utils";
 import {
   Alert,
   Container,
@@ -13,58 +13,6 @@ import {
 import { SvgViewer } from "./SvgViewer";
 import { EditorState } from "./EditorState";
 import { PoseInfoPanel } from "./PoseInfoPanel";
-
-const parseUserInput = (
-  input: string,
-  polygonSize: number
-):
-  | {
-      result: "success";
-      polygon: [number, number][];
-    }
-  | { result: "failed"; errorMessage: string } => {
-  const isPair = (pair: unknown): pair is [number, number] => {
-    return (
-      typeof pair === "object" &&
-      Array.isArray(pair) &&
-      pair.length === 2 &&
-      typeof pair[0] === "number" &&
-      typeof pair[1] === "number"
-    );
-  };
-  const isPolygon = (pairs: unknown[]): pairs is [number, number][] => {
-    return pairs.every((pair) => isPair(pair));
-  };
-
-  try {
-    const result: unknown = JSON.parse(input);
-    if (
-      result &&
-      typeof result === "object" &&
-      "vertices" in result &&
-      hasOwnProperty(result, "vertices") &&
-      typeof result.vertices === "object" &&
-      Array.isArray(result.vertices) &&
-      isPolygon(result.vertices) &&
-      result.vertices.length === polygonSize
-    ) {
-      return {
-        result: "success",
-        polygon: result.vertices,
-      };
-    }
-    return {
-      result: "failed",
-      errorMessage: "input is not valid format",
-    };
-  } catch (e) {
-    console.error(e);
-    return {
-      result: "failed",
-      errorMessage: "parse error",
-    };
-  }
-};
 
 interface SvgEditorProps {
   problem: Problem;
--- kenkoooo/src/utils.ts
+++ kenkoooo/src/utils.ts
@@ -40,3 +40,55 @@ export const hasOwnProperty = <X extends {}, Y extends PropertyKey>(
 ): obj is X & Record<Y, unknown> => {
   return obj.hasOwnProperty(prop);
 };
+
+export const parseUserInput = (
+  input: string,
+  polygonSize: number
+):
+  | {
+      result: "success";
+      polygon: [number, number][];
+    }
+  | { result: "failed"; errorMessage: string } => {
+  const isPair = (pair: unknown): pair is [number, number] => {
+    return (
+      typeof pair === "object" &&
+      Array.isArray(pair) &&
+      pair.length === 2 &&
+      typeof pair[0] === "number" &&
+      typeof pair[1] === "number"
+    );
+  };
+  const isPolygon = (pairs: unknown[]): pairs is [number, number][] => {
+    return pairs.every((pair) => isPair(pair));
+  };
+
+  try {
+    const result: unknown = JSON.parse(input);
+    if (
+      result &&
+      typeof result === "object" &&
+      "vertices" in result &&
+      hasOwnProperty(result, "vertices") &&
+      typeof result.vertices === "object" &&
+      Array.isArray(result.vertices) &&
+      isPolygon(result.vertices) &&
+      result.vertices.length === polygonSize
+    ) {
+      return {
+        result: "success",
+        polygon: result.vertices,
+      };
+    }
+    return {
+      result: "failed",
+      errorMessage: "input is not valid format",
+    };
+  } catch (e) {
+    console.error(e);
+    return {
+      result: "failed",
+      errorMessage: "parse error",
+    };
+  }
+};
