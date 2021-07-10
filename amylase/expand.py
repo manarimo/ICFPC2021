cfcc7c9de321b596e68c691869dd331838aba790
Shunsuke Ohashi <amylase.inquiry@gmail.com>
2021-07-11 00:20:08 +0900

    added expanded hint

--- amylase/expand.py
+++ amylase/expand.py
@@ -18,6 +18,24 @@ def normalized_vector(fr_v, to_v):
     return [dx / n, dy / n]
 
 
+def bounding_box(points):
+    xs, ys = [], []
+    for x, y in points:
+        xs.append(x)
+        ys.append(y)
+    min_x = min(xs)
+    max_x = max(xs)
+    min_y = min(ys)
+    max_y = max(ys)
+    return min_x, max_x, min_y, max_y
+
+
+def translate(sx, sy, tx, ty, v):
+    assert sx < sy
+    assert tx < ty
+    return (ty - tx) / (sy - sx) * (v - sx) + tx
+
+
 def expand(spec):
     hole = spec["hole"]
     vertices = spec["figure"]["vertices"]
@@ -29,8 +47,8 @@ def expand(spec):
 
     weight = 1
     spring_k = 1
-    gravity = 2000
-    time_delta = 1e-3
+    gravity = 4000
+    time_delta = 1e-2
 
     positions = [p[:] for p in vertices]
     velocities = [[0, 0] for _ in vertices]
@@ -55,20 +73,33 @@ def expand(spec):
             for dim in range(2):
                 positions[i][dim] += velocities[i][dim] * time_delta
 
-        if t % 1000 == 0:
-            kinetic_energy = 0.
-            for velocity in velocities:
-                kinetic_energy += weight * (norm(velocity) ** 2) / 2
-            print(kinetic_energy)
-    rounded_positions = [[round(v) for v in p] for p in positions]
-    print(rounded_positions)
+        # if t % 1000 == 0:
+        #     kinetic_energy = 0.
+        #     for velocity in velocities:
+        #         kinetic_energy += weight * (norm(velocity) ** 2) / 2
+        #     print(kinetic_energy)
+
+    target_box = bounding_box(hole)
+    source_box = bounding_box(positions)
+
+    translated_positions = [[translate(source_box[0], source_box[1], target_box[0], target_box[1], p[0]),
+                             translate(source_box[2], source_box[3], target_box[2], target_box[3], p[1])
+                             ] for p in positions]
+    rounded_positions = [[round(v) for v in p] for p in translated_positions]
+
+    return {"vertices": rounded_positions}
 
 
-def main():
-    with open("../problems/75.json") as f:
+def main(problem_id):
+    with open(f"../problems/{problem_id}.json") as f:
         spec = json.load(f)
-        expand(spec)
+    hint = expand(spec)
+    with open(f"../hints/spring-expand/{problem_id}.json", "w") as f:
+        json.dump(hint, f)
 
 
 if __name__ == '__main__':
-    main()
+    from multiprocessing import Pool
+    pool = Pool(processes=7)
+    problem_ids = range(1, 89)
+    pool.map(main, problem_ids)
