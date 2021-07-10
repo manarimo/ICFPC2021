e2beb094a7684836657fddd4ea5de31cc1395bf6
kenkoooo <kenkou.n@gmail.com>
2021-07-10 21:36:39 +0900

    two-three-forces

--- kenkoooo/manarimo-lib/src/types.rs
+++ kenkoooo/manarimo-lib/src/types.rs
@@ -2,20 +2,20 @@ use serde::{Deserialize, Serialize};
 
 type Pair = [i64; 2];
 
-#[derive(Deserialize)]
+#[derive(Deserialize, Clone)]
 pub struct Problem {
     pub hole: Vec<Pair>,
     pub epsilon: i64,
     pub figure: Figure,
 }
 
-#[derive(Deserialize)]
+#[derive(Deserialize, Clone)]
 pub struct Figure {
     pub edges: Vec<[usize; 2]>,
     pub vertices: Vec<Pair>,
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Clone)]
 pub struct Pose {
     pub vertices: Vec<Pair>,
 }
