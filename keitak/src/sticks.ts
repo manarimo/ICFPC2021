07ca76b4625e764b411f0d75a813a3546229310e
Keita Komuro <you@example.com>
2021-07-10 19:47:52 +1000

    done

--- keitak/src/sticks.ts
+++ keitak/src/sticks.ts
@@ -1,9 +1,9 @@
 import * as fs from 'fs';
 
-const WIDTH = 15;
-const HEIGHT = 15;
+const WIDTH = 100;
+const HEIGHT = 100;
 const EDGE_MAX = 10;
-const EDGE_MIN = 5;
+const EDGE_MIN = 1;
 type JudgeInput = {
     hole: number[][],
     figure: {
@@ -38,7 +38,7 @@ const generateSticks = (width: number, height: number) => {
     };
 }
 
-const generatePosSequences = (oneSideSticks: number[]) => {
+const generatePosSequencesWeakRestrictions = (oneSideSticks: number[]) => {
     let pos = 0;
     const res: number[] = [];
     for (let i = 0; i < oneSideSticks.length; i++) {
@@ -51,6 +51,29 @@ const generatePosSequences = (oneSideSticks: number[]) => {
     }
     return res;
 }
+
+// we have 2 restrictions
+// 1. pos must be more than or equals to 0
+// 2. pos last element must be the largest number
+const generatePosSequencesStrongRestrictions = (oneSideSticks: number[]) => {
+    let pos = 0;
+    const res: number[] = [];
+    for (let i = 0; i < oneSideSticks.length; i++) {
+        if (Math.random() < 0.5 || pos - oneSideSticks[i] < 0) {
+            pos += oneSideSticks[i];
+        } else {
+            pos -= oneSideSticks[i];
+        }
+        res.push(pos);
+    }
+    const max = res.reduce((acc, val) => Math.max(acc, val), 0);
+    const lastPos = max + EDGE_MIN;
+    const lastLen = lastPos - res[res.length - 1];
+    res.push(lastPos);
+    oneSideSticks.push(lastLen);
+    return res;
+}
+
 
 const createFigure = (top: number[], right: number[]) => {
     const vertices: number[][] = [];
@@ -82,8 +105,8 @@ const createFigure = (top: number[], right: number[]) => {
 }
 
 const createAnswer = (top: number[], right: number[]): JudgeOutput => {
-    const X = generatePosSequences(top);
-    const Y = generatePosSequences(right);
+    const X = generatePosSequencesWeakRestrictions(top);
+    const Y = generatePosSequencesStrongRestrictions(right);
     const vertices: number[][] = [];
     for (let i = 0; i < X.length; i++) {
         vertices.push([X[i], 0]);
@@ -91,12 +114,14 @@ const createAnswer = (top: number[], right: number[]): JudgeOutput => {
     for (let i = 0; i < Y.length; i++) {
         vertices.push([X[X.length - 1], Y[i]]);
     }
-    for (let i = X.length - 1; i >= 0; i--) {
+    // wow not intuitive
+    for (let i = X.length - 2; i >= 0; i--) {
         vertices.push([X[i], Y[Y.length - 1]]);
     }
     for (let i = Y.length - 1; i >= 0; i--) {
         vertices.push([0, Y[i]]);
     }
+    vertices.push([0, 0]);
     return {
         vertices
     };
@@ -120,14 +145,17 @@ const createHole = (vertices: number[][]) => {
 
 const generate = () => {
     const {top, right} = generateSticks(WIDTH, HEIGHT);
-    const figure = createFigure(top, right);
+    // WARNING! right can be modified by createAnswer
     const answer = createAnswer(top, right);
+    const figure = createFigure(top, right);
     const hole = createHole(answer.vertices);
     const input: JudgeInput = {
         hole, figure, epsilon: 0,
     }
-    fs.writeFile('./p_keita.json', JSON.stringify(input), () => {});
-    fs.writeFile('./a_keita.json', JSON.stringify(answer), () => {});
+    const vs = figure.vertices.length;
+    console.log(vs);
+    fs.writeFile(`./problems/${WIDTH}_${HEIGHT}_${vs}.json`, JSON.stringify(input), () => {});
+    fs.writeFile(`./solutions/${WIDTH}_${HEIGHT}_${vs}.json`, JSON.stringify(answer), () => {});
 }
 
 generate();
