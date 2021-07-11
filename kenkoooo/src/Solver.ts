136b3d0ec4b946ba354c8a7423dfdaf32ff2cab3
Shunsuke Ohashi <amylase.inquiry@gmail.com>
2021-07-11 17:25:48 +0900

    improved solveSinglePoint

--- kenkoooo/src/Solver.ts
+++ kenkoooo/src/Solver.ts
@@ -1,21 +1,19 @@
 import { Problem } from "./utils";
 import { isEdgeInside, isPointInside, isValidEdge } from "./tslib/amyfunc";
 
-const THRESHOLD = 2500;
+const THRESHOLD = 100;
 
 const D = [
   [1, -1],
   [1, 1],
-  [-1, 1],
-  [-1, -1],
 ];
 
 const toPoint = ([x, y]: [number, number]) => ({ x, y });
 
 export const solveSinglePoint = (
-  pose: [number, number][],
-  problem: Problem,
-  targetPointId: number
+    pose: [number, number][],
+    problem: Problem,
+    targetPointId: number
 ) => {
   const holePolygon = problem.hole.map(([x, y]) => ({ x, y }));
   const originalPose = problem.figure.vertices;
@@ -29,37 +27,40 @@ export const solveSinglePoint = (
   });
 
   const [curX, curY] = pose[targetPointId];
-  for (let distance = 1; distance < THRESHOLD; distance++) {
-    let x = curX - distance;
-    let y = curY;
+  for (let distance = 0; distance < THRESHOLD; distance++) {
+    for (let xDirection of [1, -1]) {
+      let x = curX - distance * xDirection;
+      let y = curY;
 
-    for (let [dx, dy] of D) {
-      for (let i = 0; i < distance; i++) {
-        x += dx;
-        y += dy;
-        if (!isPointInside(holePolygon, { x, y })) {
-          continue;
-        }
+      for (let [dx, dy] of D) {
+        for (let i = 0; i < distance; i++) {
+          x += dx * xDirection;
+          y += dy;
+          if (!isPointInside(holePolygon, { x, y })) {
+            continue;
+          }
 
-        const targetPoint = { x, y };
-        const edgeCheck = edges.every(([i, j]) => {
-          const piOriginal = toPoint(originalPose[i]);
-          const pjOriginal = toPoint(originalPose[j]);
-          const pi = i === targetPointId ? targetPoint : toPoint(pose[i]);
-          const pj = j === targetPointId ? targetPoint : toPoint(pose[j]);
-          const newEdge = { src: pi, dst: pj };
-          return (
-            isValidEdge(
-              { src: piOriginal, dst: pjOriginal },
-              newEdge,
-              epsilon
-            ) && isEdgeInside(holePolygon, newEdge)
-          );
-        });
-        if (edgeCheck) {
-          return targetPoint;
+          const targetPoint = { x, y };
+          const edgeCheck = edges.every(([i, j]) => {
+            const piOriginal = toPoint(originalPose[i]);
+            const pjOriginal = toPoint(originalPose[j]);
+            const pi = i === targetPointId ? targetPoint : toPoint(pose[i]);
+            const pj = j === targetPointId ? targetPoint : toPoint(pose[j]);
+            const newEdge = { src: pi, dst: pj };
+            return (
+                isValidEdge(
+                    { src: piOriginal, dst: pjOriginal },
+                    newEdge,
+                    epsilon
+                ) && isEdgeInside(holePolygon, newEdge)
+            );
+          });
+          if (edgeCheck) {
+            return targetPoint;
+          }
         }
       }
     }
+
   }
 };
