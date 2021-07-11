c049e94f0952e5be80b884434689da4e4dfd169a
kenkoooo <kenkou.n@gmail.com>
2021-07-11 23:47:13 +0900

    kenkoooo: brute-force 2

--- kenkoooo/brute-force/src/bin/two-three-forces.rs
+++ kenkoooo/brute-force/src/bin/two-three-forces.rs
@@ -1,57 +1,151 @@
-use brute_force::amylase_bruteforce;
+use anyhow::Result;
+use brute_force::{amylase_bruteforce, PathBufExt};
+use manarimo_lib::geometry::{dislike, Point};
 use manarimo_lib::types::{Pose, Problem};
+use rayon::prelude::*;
+use std::collections::HashMap;
 use std::env;
-use std::error::Error;
-use std::fs::File;
-use std::io::{BufReader, BufWriter};
-use std::path::PathBuf;
+use std::fs::{read_dir, File};
+use std::io::BufWriter;
+use std::path::{Path, PathBuf};
 
-fn main() -> Result<(), Box<dyn Error>> {
+fn main() -> Result<()> {
     let args = env::args().collect::<Vec<_>>();
 
-    let file = File::open(&args[1])?;
-    let path = PathBuf::from(&args[1]);
-    let path = path
-        .file_name()
-        .expect(&format!("{} is not a file", args[1]));
-    let reader = BufReader::new(file);
-    let problem: Problem = serde_json::from_reader(reader)?;
-    let output_dir = PathBuf::from(&args[2]);
-    let output = output_dir.join(path);
+    let problem_dir = &args[1];
+    let solution_dir = &args[2];
+    let output_dir = PathBuf::from(&args[3]);
 
-    let file = File::open(&args[3])?;
-    let reader = BufReader::new(file);
-    let mut solution: Pose = serde_json::from_reader(reader)?;
+    let problems = fetch_all_problems(problem_dir)?;
+    let mut solutions = fetch_all_submission_files(solution_dir)?;
 
-    let n = solution.vertices.len();
-    let mut best_dislike = 1 << 60;
-    eprintln!("3 forces");
-    for (i, j, k) in (0..n)
-        .flat_map(|i| (0..i).map(move |j| (i, j)))
-        .flat_map(|(i, j)| (0..j).map(move |k| (i, j, k)))
-    {
-        let fixed = (0..n)
-            .filter(|&x| x != i && x != j && x != k)
+    let mut best_solutions = HashMap::new();
+    for (problem_id, problem) in problems {
+        let hole = problem
+            .hole
+            .iter()
+            .cloned()
+            .map(Point::from)
             .collect::<Vec<_>>();
-        amylase_bruteforce::solve(
-            problem.clone(),
-            &fixed,
-            solution.clone(),
-            |pose, dislike| {
-                if dislike < best_dislike {
-                    let file = File::create(&output).expect("file creation error");
-                    let writer = BufWriter::new(file);
-                    serde_json::to_writer(writer, &pose).expect("write error");
-                    println!("{:?} dislike:{}", output, dislike);
-                    best_dislike = dislike;
-                    solution = pose;
+        if let Some(solutions) = solutions.remove(&problem_id) {
+            for solution in solutions {
+                let pose = solution
+                    .vertices
+                    .iter()
+                    .cloned()
+                    .map(Point::from)
+                    .collect::<Vec<_>>();
+                let dislike = dislike(&hole, &pose);
+                let current = best_solutions
+                    .entry(problem_id)
+                    .or_insert_with(|| (dislike, problem.clone(), solution.clone()));
+                if current.0 > dislike {
+                    current.0 = dislike;
+                    current.2 = solution;
                 }
-            },
-            |_| {
-                // do nothing
-            },
-        );
+            }
+        }
     }
 
+    best_solutions.into_par_iter().for_each(
+        |(problem_id, (dislike, problem, mut solution)): (i64, (i64, Problem, Pose))| {
+            println!("Solving {}", problem_id);
+            let output = output_dir.join(format!("{}.json", problem_id));
+            let mut best_dislike = dislike;
+            let fixed_lists = get_fixed_lists(problem.figure.vertices.len(), 2);
+            for fixed in fixed_lists {
+                amylase_bruteforce::solve(
+                    problem.clone(),
+                    &fixed,
+                    solution.clone(),
+                    |pose, dislike| {
+                        if dislike < best_dislike {
+                            let file = File::create(&output).expect("file creation error");
+                            let writer = BufWriter::new(file);
+                            serde_json::to_writer(writer, &pose).expect("write error");
+                            println!("{:?} dislike:{}", output, dislike);
+                            best_dislike = dislike;
+                            solution = pose;
+                        }
+                    },
+                    |_| {
+                        // do nothing
+                    },
+                );
+            }
+            println!("Solved {}", problem_id);
+        },
+    );
+
     Ok(())
+}
+
+fn get_fixed_lists(n: usize, force: usize) -> Vec<Vec<usize>> {
+    if force == 2 {
+        (0..n)
+            .flat_map(|i| (0..i).map(move |j| (i, j)))
+            .map(|(i, j)| (0..n).filter(|&x| x != i && x != j).collect::<Vec<_>>())
+            .collect()
+    } else if force == 3 {
+        (0..n)
+            .flat_map(|i| (0..i).map(move |j| (i, j)))
+            .flat_map(|(i, j)| (0..j).map(move |k| (i, j, k)))
+            .map(|(i, j, k)| {
+                (0..n)
+                    .filter(|&x| x != i && x != j && x != k)
+                    .collect::<Vec<_>>()
+            })
+            .collect()
+    } else {
+        unimplemented!();
+    }
+}
+
+fn fetch_all_submission_files<P: AsRef<Path>>(dir: P) -> Result<HashMap<i64, Vec<Pose>>> {
+    let mut solutions = HashMap::new();
+    for path in read_dir(dir)? {
+        let dir_path = path?.path();
+        if !dir_path.is_dir() {
+            continue;
+        }
+
+        for filepath in read_dir(dir_path)? {
+            let filepath = filepath?.path();
+            if !filepath.is_json()? {
+                continue;
+            }
+
+            println!("Loading {:?}", filepath);
+            let problem_id = match filepath.problem_id() {
+                Ok(problem_id) => problem_id,
+                Err(_) => continue,
+            };
+
+            let solution = match filepath.parse_json::<Pose>() {
+                Ok(solution) => solution,
+                Err(_) => continue,
+            };
+
+            solutions
+                .entry(problem_id)
+                .or_insert_with(Vec::new)
+                .push(solution);
+        }
+    }
+    Ok(solutions)
+}
+
+fn fetch_all_problems<P: AsRef<Path>>(dir: P) -> Result<HashMap<i64, Problem>> {
+    let mut map = HashMap::new();
+    for path in read_dir(dir)? {
+        let filepath = path?.path();
+        if !filepath.is_json()? {
+            continue;
+        }
+        println!("Loading {:?}", filepath);
+        let problem_id = filepath.problem_id()?;
+        let problem: Problem = filepath.parse_json()?;
+        map.insert(problem_id, problem);
+    }
+    Ok(map)
 }
--- kenkoooo/brute-force/src/lib.rs
+++ kenkoooo/brute-force/src/lib.rs
@@ -1,1 +1,41 @@
+use anyhow::{Context, Result};
+use serde::de::DeserializeOwned;
+use std::fs::File;
+use std::io::BufReader;
+use std::path::PathBuf;
+
 pub mod amylase_bruteforce;
+
+pub trait PathBufExt {
+    fn is_json(&self) -> Result<bool>;
+    fn problem_id(&self) -> Result<i64>;
+    fn parse_json<T: DeserializeOwned>(&self) -> Result<T>;
+}
+
+impl PathBufExt for PathBuf {
+    fn is_json(&self) -> Result<bool> {
+        if self.is_file() {
+            let extension = self
+                .extension()
+                .and_then(|s| s.to_str())
+                .context("no extension")?;
+            Ok(extension == "json")
+        } else {
+            Ok(false)
+        }
+    }
+
+    fn problem_id(&self) -> Result<i64> {
+        let problem_id = self
+            .file_stem()
+            .and_then(|s| s.to_str())
+            .context("no file name")?
+            .parse::<i64>()?;
+        Ok(problem_id)
+    }
+    fn parse_json<T: DeserializeOwned>(&self) -> Result<T> {
+        let file = File::open(self)?;
+        let solution: T = serde_json::from_reader(BufReader::new(file))?;
+        Ok(solution)
+    }
+}
