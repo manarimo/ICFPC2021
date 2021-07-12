0552751996db21516937946cda61d3ca7f78535e
Ubuntu <ubuntu@ip-172-31-83-185.ec2.internal>
2021-07-12 09:27:24 +0000

    infinite

--- kawatea/infinite_train.rb
+++ kawatea/infinite_train.rb
@@ -1,7 +1,12 @@
 require 'fileutils'
+require_relative '../ruby-lib/problem'
 
 solution_base = ARGV[0]
 start_temps = [10, 100, 1000, 10000]
+edge_penalties = [1000, 5000, 10000]
+
+solutions = Problem::load_solutions
+valuable_problems = Problem::valuable_problems(solutions)
 
 current_dir = "#{__dir__}/../hints/#{solution_base}"
 loop_count = 1
@@ -17,25 +22,27 @@ loop do
   FileUtils.makedirs(next_dir)
 
   start_temp = start_temps.sample
+  edge_penalty = edge_penalties.sample
   Dir.glob("#{current_dir}/*.json") do |file|
     next if file.match(/_verdict.json/)
     id = file.match(/(\d+).json/)[1].to_i
+    next unless valuable_problems.include?(id)
 
     hint_path = "#{current_dir}/#{id}.json"
     out_path = "#{next_dir}/#{id}.json"
     log_path = "#{next_dir}/#{id}.log"
-    cmdline = "./a.out -s #{start_temp} -t 600 /dev/null #{hint_path} < ../problems/#{id}.json > #{out_path} 2> #{log_path} &"
+    cmdline = "./a.out -s #{start_temp} -t 300 -e #{edge_penalties} /dev/null #{hint_path} < ../problems/#{id}.json > #{out_path} 2> #{log_path} &"
 
     puts cmdline
     system(cmdline)
 
     process_count += 1
-    if process_count % 71 == 0
-      sleep 630
+    if process_count % 95 == 0
+      sleep 320
     end
   end
 
-  sleep 630
+  sleep 320
 
   loop_count += 1
   current_dir = next_dir
