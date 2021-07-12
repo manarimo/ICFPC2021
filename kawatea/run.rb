05b551497ca00849b60c55ba441831bd76c40948
Osamu Koga <osak.63@gmail.com>
2021-07-12 03:33:50 -0500

    暴力用

--- kawatea/run.rb
+++ kawatea/run.rb
@@ -1,21 +1,26 @@
-prob_ids = (106..132).to_a
+require_relative '../ruby-lib/problem'
+
+prob_ids = (1..132).to_a
 hints = %w(rot-0 rot-90 rot-180 rot-270 flip-rot-0 flip-rot-90 flip-rot-180 flip-rot-270).map { |w| "raw-" + w }
 #hints = %w(spring-expand)
 
+solutions = Problem::load_solutions
+valueable_problems = Problem::valueable_problems(solutions)
+
 count = 0
-program = ARGV[0]
-name = ARGV[1]
+name = ARGV[0]
 hints.each do |hint|
   dirname = "../solutions/#{name}-#{hint}"
   `mkdir -p '#{dirname}'`
   prob_ids.each do |id|
+    next unless valueable_problems.include?(id)
     if hint == 'nohint'
       hint_path = ''
     else
       hint_path = "../hints/#{hint}/#{id}.json"
     end
 
-    cmdline = "#{program} /dev/null #{hint_path} < ../problems/#{id}.json > #{dirname}/#{id}.json 2> #{dirname}/#{id}.log &"
+    cmdline = "#{program} -s 10000 -t 600 /dev/null #{hint_path} < ../problems/#{id}.json > #{dirname}/#{id}.json 2> #{dirname}/#{id}.log &"
     puts cmdline
     system(cmdline)
 
